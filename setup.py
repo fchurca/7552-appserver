b3325f616be4d8dd3fd14187a1dff552439d4312
adrian-mb <adrian.mb.fiub@gmail.com>
2017-08-26 20:46:32 -0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from pip.req import parse_requirements
 from setuptools import setup, find_packages
 
-requirements = parse_requirements('./requirements.txt', session=False)
+requirements = parse_requirements('requirements.txt', session=False)
 requirements = [str(r.req) for r in requirements]
 
-package_dir = {'', 'src'}
 
 setup(name='appserver', 
-      version='0.1', 
-      packages=find_packages(),
+      version='0.1',
+      package_dir = {'': 'src'}, 
+      packages=find_packages('src'),
+      data_files=[('', ['requirements.txt'])],
       install_requires=requirements)
