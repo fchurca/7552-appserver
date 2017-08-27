70611140df0ef5b16edcf151abbcf286110bc812
adrian-mb <adrian.mb.fiub@gmail.com>
2017-08-26 22:20:15 -0300

    Implement Doxygen

--- src/appserver/applog/__init__.py
+++ src/appserver/applog/__init__.py
@@ -1,9 +1,15 @@
+## @package appserver.applog
+#
+# Define clases y utilidades para estandarizar el proceso de logging.
+
 from appserver import appconfig
 
 import logging
 import sys
 
-
+##
+# Clase encargada de administrar la instanciación y configuración de los objetos
+# Logger a ser utilizados por las demás entidades del sistema.
 class LoggerFactory(object):
     
     _CONFIG = appconfig.APP_CONFIG.get_file('logging.ini')
@@ -14,5 +20,8 @@ class LoggerFactory(object):
     
     logging.basicConfig(level=_LEVEL, format=_FORMAT)
     
+    ##
+    # @param name Nombre que identifica al logger.
+    # @return Instancia de un Logger con el nombre recibido como argumento. 
     def getLogger(self, name):
         return logging.getLogger(name)
