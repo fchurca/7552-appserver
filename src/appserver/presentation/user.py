45c8d092c3df855b1fae6e47fc436ff4c18006b8
Federico Churca-Torrusio <federico.churca@gmail.com>
2017-10-30 10:48:42 -0300

    user/:POST: extract username from token for existing users

--- src/appserver/presentation/user.py
+++ src/appserver/presentation/user.py
@@ -38,37 +38,41 @@ class UserResource(Resource):
     def post(self):
         logger.info('method:POST')
         content = request.get_json()
-        if 'username' not in content:
-            return 'Malformed request', 400
-        key = content['username']
-        user = repository.find_one(key)
-        if (user):
-            if (not 'Authorization' in request.headers):
-                logger.warn('no authorizationHeader')
-                return 'Missing Authorization header', 401
+        if (not 'Authorization' in request.headers):
+            logger.info('No authorizationheader; new user')
+            username=content['username']
+            if (repository.find_one(username)):
+                logger.warn('User exists')
+                return 'User already exists', 409
+            else:
+                logger.debug('User didn\'t exist')
+                if repository.insert(content):
+                    logger.debug('Success')
+                    return repository.find_one(username, {'token': False, 'password': False}), 202
+                else:
+                    logger.debug('Error')
+                    return 'There was an error creating the user', 500
+        else:
             authorizationHeader=request.headers['Authorization']
             logger.info('Authorization header: %s', authorizationHeader)
             if (not re.match(re.compile(r'^bearer\W', re.I), authorizationHeader)):
                 logger.warn('no bearer')
                 return 'Authorization header is not Bearer', 401
             tokenpayload = b64decode(re.compile(r'^bearer\s+(.*)$', re.I).sub(r'\1',authorizationHeader))
-            tusername, token = tokenpayload.decode('ascii').split('|')
-            logger.info('Proposed token: %s', tokenpayload)
-            logger.info('User token: %s', user['token'])
-            logger.info('existing user')
+            logger.info('Tokenpayload: %s', tokenpayload)
+            username,token=tokenpayload.decode('ascii').split('|')
+            logger.info('Username: %s', username)
+            logger.info('Token: %s', token)
+            user = repository.find_one(username, {'password': False})
+            if user['username'] != username:
+                logger.info('wrong username')
+                return 'Wrong username', 403
             if user['token'] != tokenpayload:
                 logger.info('wrong token')
                 return 'Session expired', 401
+            logger.info('update user')
+            if repository.update(username, content):
+                return repository.find_one(username, {'token': False, 'password': False}), 202
             else:
-                logger.info('update user')
-                if repository.update(key, content):
-                    return repository.find_one(key, {'token': False, 'password': False}), 202
-                else:
-                    return None, 500
-        else:
-            logger.info('new user')
-            if repository.insert(content):
-                return repository.find_one(key), 201
-            else:
-                return None, 500
+                return "There was an error processing the request", 500
 
